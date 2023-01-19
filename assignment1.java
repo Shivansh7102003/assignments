d6aa420fd947e4a5634cf3fdeccb9eee28cf4daa
Shivansh7102003 <123052704+Shivansh7102003@users.noreply.github.com>
2023-01-19 15:52:45 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,38 @@
+public class assignment1 {
+    //    Question 1
+    public static void main(String[] args) {
+        int x=2;
+        int y=4;
+        System.out.println(x*y);
+//        Question 2
+        int a= 'U';
+        System.out.println("the ascii value of U is : " +a);
+//        Question 3
+        int lenght = 5;
+        int breadth = 6;
+        System.out.println("the lenght is :" +lenght);
+        System.out.println("the breadth is :" +breadth);
+        int area = lenght*breadth;
+        System.out.println("the area is :" +area);
+//        Question 4
+        int b=2;
+        int cube = b*b*b;
+        System.out.println("the side is :" +b);
+        System.out.println("the cube is :" +cube);
+//      Question 5
+        int num1 = 2;
+        int num2 = 3;
+        System.out.println("the first number is :" +num1);
+        System.out.println("the second number is :" +num2);
+        int temp; // variable used to swap numbers
+        temp=num1;
+        num1=num2;
+        num2=temp;
+        System.out.println("the first number is:" +num1);
+        System.out.println("the second number is :" +num2);
+
+
+    }
+
+
+}
